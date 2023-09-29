5d99ec45180a4f7a33de9e2051908f0b22b48d92
ImranRH321 <imranrh247@gmail.com>
2023-09-29 12:00:49 +0600

    amdin home state

--- src/pages/Dashboard/AdminDashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminDashboard/AdminHome/AdminHome.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import useAuth from '../../../../hooks/useAuth';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const AdminHome = () => {
     const { currentUser } = useAuth();
+    const { instanceSecoreApis } = useAxiosSecure();
+
+    const {data: state = {}, } = useQuery({
+        queryKey: ['/admin/state'],
+        queryFn: async () => {
+            const res = await instanceSecoreApis(`/admin/state`)
+            console.log('cart get  res me: ', res);
+            return res.data;
+        },
+    })
+    console.log('data me admin ==========', state);
     return (
         <div className='w-full m-4'>
             <h1 className='text-3xl'>Welcome back, {currentUser?.displayName}</h1>
--- src/pages/Dashboard/UsersDashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UsersDashboard/UserHome/UserHome.jsx
@@ -3,6 +3,8 @@ import useAuth from '../../../../hooks/useAuth';
 
 const UserHome = () => {
     const { currentUser } = useAuth();
+
+
     return (
         <div className='w-full m-4'>
             <h1 className='text-3xl'>Welcome back, {currentUser?.displayName}</h1>
